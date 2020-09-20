81d871acf3601e5bcb190bfdfc99a50bb316b26d
rupeshrahate05 <71579311+rupeshrahate05@users.noreply.github.com>
2020-09-20 10:23:47 +0530

    first commit

---
+++ my_first_java_program/src/myPackage/MyJavaClass.java
@@ -0,0 +1,34 @@
+package myPackage;
+public class MyJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		System.out.println("Hello World;its my first step in the programming!!!");
+
+		int x= 100;
+		System.out.println("value of x: " +x);
+		float f1=88.1f;
+		System.out.println("value of f1:" +f1);
+
+		float f2 = x;
+		int i2=(int) f1;
+		short s2= 97;
+		System.out.println("value f2:" +f2);
+		System.out.println("value i2+:" +i2);
+
+		char c1 = 'a';
+		int i3= c1;
+		char c2= (char) x;
+		char c3= (char) f1;
+		short s1= (short) c1;
+		char c4=(char)s2;
+		System.out.println("value i3+:" +i3 );
+		System.out.println("value c2+:" +c2 );
+		System.out.println("value s1:" +s1);
+		System.out.println("value c4:" +c4 );
+
+	}
+
+}
